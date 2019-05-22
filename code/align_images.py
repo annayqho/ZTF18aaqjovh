2003034a99ebbda78b61dca1575fac7fe825894d
Anna Ho <annayqho@gmail.com>
2019-05-22 16:14:46 -0700

    align u as well

--- code/align_images.py
+++ code/align_images.py
@@ -8,6 +8,7 @@ gfits = "../data/host_sdss/frame-g-005116-5-0061.fits"
 rfits = "../data/host_sdss/frame-r-005116-5-0061.fits"
 ifits = "../data/host_sdss/frame-i-005116-5-0061.fits"
 zfits = "../data/host_sdss/frame-z-005116-5-0061.fits"
+ufits = "../data/host_sdss/frame-u-005116-5-0061.fits"
 
 # centering
 ra = 178.181754 
