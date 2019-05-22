3f6eeff7342df187f5432f9a4743dfd4b5db8d5f
Anna Ho <annayqho@gmail.com>
2019-05-22 15:55:05 -0700

    align images

--- code/align_images.py
+++ code/align_images.py
@@ -7,10 +7,16 @@ swarp_path = '/usr/local/optical/swarp/bin/swarp'
 gfits = "../data/host_sdss/frame-g-005116-5-0061.fits"
 rfits = "../data/host_sdss/frame-r-005116-5-0061.fits"
 ifits = "../data/host_sdss/frame-i-005116-5-0061.fits"
+zfits = "../data/host_sdss/frame-z-005116-5-0061.fits"
+
+# centering
+ra = 178.181754 
+dec = 25.675033
+size = 600
 
 swarp_command = swarp_path \
-        + " g.fits r.fits i.fits -c config.swarp -CENTER'" \
-        + ra + " " + dec \
-        + "; -SUBTRACT_BACK Y -RESAMPLE Y -COMBINE Y -IMAGE_SIZE'" \
-        + '1700' + "," + '1700' + "'"
+        + " %s %s %s -c config.swarp -CENTER '" %(gfits,rfits,zfits)\
+        + str(ra) + " " + str(dec) \
+        + "' -SUBTRACT_BACK Y -RESAMPLE Y -COMBINE N -IMAGE_SIZE '" \
+        + str(size) + "," + str(size) + "'"
 os.system(swarp_command)
