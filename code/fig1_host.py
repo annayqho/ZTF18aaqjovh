67f1b11d6519c7c06910ed7687ff20bcb51759be
annayqho <annayqho@gmail.com>
2019-05-22 15:57:26 -0700

    a passable version

--- code/fig1_host.py
+++ code/fig1_host.py
@@ -13,14 +13,16 @@ dec = 25.6750332
 
 ddir = "/Users/annaho/Dropbox/Projects/Research/ZTF18aaqjovh/data/host_sdss"
 
-gim = fits.open(ddir + "/frame-g-005116-5-0061.fits.bz2")
+gim = fits.open(ddir + "/frame-g-005116-5-0061.resamp.fits")
 g = gim[0].data # 0 to 0.1 looks good
-u = fits.open( 
-        ddir + "/frame-u-005116-5-0061.fits.bz2")[0].data
 r = fits.open( # 0 to 0.2 looks good
-        ddir + "/frame-r-005116-5-0061.fits.bz2")[0].data
+        ddir + "/frame-r-005116-5-0061.resamp.fits")[0].data
 z = fits.open( # 0 to 0.6 looks good
-        ddir + "/frame-z-005116-5-0061.fits.bz2")[0].data
+        ddir + "/frame-z-005116-5-0061.resamp.fits")[0].data
+u = fits.open( 
+        ddir + "/frame-u-005116-5-0061.resamp.fits")[0].data
+i = fits.open( 
+        ddir + "/frame-i-005116-5-0061.resamp.fits")[0].data
 
 # Figure out pos from header
 head = gim[0].header
@@ -29,12 +31,22 @@ target_pix = wcs.all_world2pix([(np.array([ra,dec], np.float_))], 1)[0]
 xpos = target_pix[0]
 ypos = target_pix[1]
  
+imsize = 120
 # Plot cutout
-im = g[int(ypos-100):int(ypos+100),int(xpos-100):int(xpos+100)]
-rgb = make_lupton_rgb(
-        z/6, r/2, g, Q=10, stretch=0.5)
-plt.imshow(
-        rgb[int(ypos-100):int(ypos+100),int(xpos-100):int(xpos+100)], 
-        origin='lower')
+gcut = g[int(ypos-imsize):int(ypos+imsize),int(xpos-imsize):int(xpos+imsize)]
+rcut = r[int(ypos-imsize):int(ypos+imsize),int(xpos-imsize):int(xpos+imsize)]
+zcut = z[int(ypos-imsize):int(ypos+imsize),int(xpos-imsize):int(xpos+imsize)]
+ucut = u[int(ypos-imsize):int(ypos+imsize),int(xpos-imsize):int(xpos+imsize)]
+icut = i[int(ypos-imsize):int(ypos+imsize),int(xpos-imsize):int(xpos+imsize)]
 
-plt.show()
+# icut: vmin = 0, vmax = 0.5
+# rcut: vmin = 0, vmax = 0.3
+# gcut: vmin = 0, vmax = 0.2
+
+rgb = make_lupton_rgb(
+        icut/2, rcut/1.5, gcut, Q=4, stretch=0.2)
+plt.imshow(rgb, origin='lower')
+
+#plt.show()
+plt.savefig("host.eps", format='eps', dpi=1000)
+
