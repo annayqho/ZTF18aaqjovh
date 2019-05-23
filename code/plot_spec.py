50a0ee98a7f86acc705f1a1d3c5d011123d2ec36
annayqho <annayqho@gmail.com>
2019-05-23 10:23:21 -0700

    rough spectral sequence

--- code/plot_spec.py
+++ code/plot_spec.py
@@ -2,6 +2,7 @@
 """
 
 import matplotlib.pyplot as plt
+import numpy as np
 from matplotlib import rc
 rc("font", family="serif")
 rc("text", usetex=True)
@@ -10,6 +11,11 @@ from ztfquery import query
 from ztfquery import marshal
 import extinction
 import glob
+from astropy.time import Time
+
+
+z = 0.03154
+SPEC_DIR = "Data/marshal/spectra/ZTF18aaqjovh"
 
 
 def download_spec():
@@ -28,54 +34,97 @@ def download_spec():
     return f
 
 
-def load_marshal_lc():
-    f = "Data/marshal/lightcurves/ZTF18aaqjovh/marshal_plot_lc_lightcurve_ZTF18aaqjovh.csv"
-    lc = ascii.read(f)
-    det = lc['mag'] < 99
-    dt = lc['jdobs'][det]-lc['jdobs'][det][0]
-    mag = lc['mag'][det]
-    emag = lc['emag'][det]
-    dt_nondet = lc['jdobs'][~det]-lc['jdobs'][det][0]
-    lm = lc['limmag'][~det]
+def get_files(sind, eind):
+    """ 
+    start_ind: starting index
+    end_ind: end index
+    """
+    files = np.array(glob.glob(SPEC_DIR + "/*.ascii"))
+    dt = np.zeros(len(files))
+    tels = []
+    cols = np.array([""]*len(dt), dtype='U10')
+
+    # Read in all of the files, pull out the corresponding dates, and sort by date
+    t0 = 58233.17615 # in MJD
+    for ii,f in enumerate(files):
+        tel = f.split("_")[2]
+        tels.append(tel)
+        alldat = open(f).readlines()
+        if tel == 'LT': 
+            for line in alldat:
+                if 'DATE-OBS' in line:
+                    obsdate = line[13:36]
+                    t = Time(obsdate, format='isot').mjd
+                    dt[ii] = t-t0
+            cols[ii] = 'magenta'
+        elif tel == 'P200':
+            for line in alldat:
+                if 'UTSHUT' in line:
+                    obsdate = line[11:]
+                    t = Time(obsdate, format='isot').mjd
+                    dt[ii] = t-t0
+            cols[ii] = 'lightblue'
+        elif tel == 'Keck1':
+            for line in alldat:
+                if 'DATE_BEG' in line:
+                    obsdate = line[13:32]
+                    t = Time(obsdate, format='isot').mjd
+                    dt[ii] = t-t0
+            cols[ii] = 'red'
+        elif tel == 'DCT':
+            obsdate = '2018-09-14T00:00:00' # temporary
+            t = Time(obsdate, format='isot').mjd
+            dt[ii] = t-t0
+            cols[ii] = 'yellow'
+        elif tel == 'NOT':
+            obsdate = '2018-09-17T00:00:00' # temporary
+            t = Time(obsdate, format='isot').mjd
+            dt[ii] = t-t0
+            cols[ii] = 'green'
+        elif tel == 'P60':
+            for line in alldat:
+                if 'OBSUTC' in line:
+                    temp = line[10:].split(" ")
+                    obsdate = temp[0] + "T" + temp[1]
+                    t = Time(obsdate, format='isot').mjd
+                    dt[ii] = t-t0
+            cols[ii] = 'black'
+        else:
+            print("couldn't find telescope")
+            print(tel)
+    order = np.argsort(dt)
+    files_sorted = files[order]
+    dt_sorted = dt[order]
+    tel_sorted = np.array(tels)[order]
+    cols = cols[order]
+    return files_sorted[sind:eind], dt_sorted[sind:eind], tel_sorted[sind:eind]
 
 
-def load_danny_lc():
-    data_dir = "/Users/annaho/Dropbox/Projects/Research/ZTF18aaqjovh/data"
-    f = data_dir + "/ZTF18aaqjovh.csv"
-    lc = ascii.read(f)
-    det = lc['mag'] < 99
-    dt = lc['mjd'][det]-lc['mjd'][det][0]
-    mag = lc['mag'][det]
-    emag = lc['magerr'][det]
-    dt_nondet = lc['mjd'][~det]-lc['mjd'][det][0]
-    lm = lc['lim_mag'][~det]
-    
-    fig = plt.figure(figsize=(7, 4))
-
-    # Plot r-band light curve
-    choose = lc['filter'][det] == 'ztfr'
-    plt.errorbar(
-            dt[choose], mag[choose], yerr=emag[choose], 
-            fmt='o', c='k', label="$r$")
-
-    # Plot r-band upper limits
-    choose = lc['filter'][~det] == 'ztfr'
-    plt.errorbar(
-            dt_nondet[choose], lm[choose], fmt='v', c='k', label=None)
-
-    plt.xlabel("Days Since First ZTF Detection", fontsize=16)
-    plt.ylabel("Apparent Mag (AB)", fontsize=16)
-    plt.ylim(17.9,21)
-    plt.xlim(-22,32)
-    plt.gca().invert_yaxis()
-    plt.tick_params(axis='both', labelsize=14)
-    fig.text(0.94, 0.5, "Absolute Magnitude (AB)",
-             ha='center', va='center', fontsize=16, rotation='vertical')
-    plt.tight_layout()
-    plt.legend(loc='upper right', fontsize=14)
-    plt.show()
-    #plt.savefig('lc.eps', format='eps', dpi=1000)
+def load_spec(f):
+    lc = np.loadtxt(f)
+    wl = lc[:,0]
+    f = lc[:,1]
+    return wl, f
 
 
 if __name__=="__main__":
-    download_spec()
+    fig,ax = plt.subplots()
+    files, epochs, tels = get_files(0, 6)
+    nfiles = len(files)
+    shift = [0, 1, 1.5, 2.5, 3.5, 5, 6, 7, 8]
+    for ii,f in enumerate(files):
+        tel = tels[ii]
+        dt = epochs[ii]
+        wl, flux = load_spec(f)
+        scale = flux[wl>4100][0]
+        shifted = flux/scale-shift[ii]
+        plt.plot(
+            wl, shifted-shift[ii], c='lightgrey', 
+            drawstyle='steps-mid', lw=0.4, alpha=1.0)
+
+    plt.tick_params(axis='both', labelsize=14)
+    plt.xlim(3600, 9500)
+    plt.ylim(-10, 0)
+    plt.xlabel(r"Observed Wavelength (\AA)", fontsize=16)
+    plt.ylabel(r"Scaled $F_{\lambda}$ + const.", fontsize=16)
+    plt.show()
